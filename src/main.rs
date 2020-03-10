3012ace2e38a925e63d5e6584494e188aa8020d3
freesig <tomrgowan@gmail.com>
2020-03-10 11:27:27 +1100

    tokio aware

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,58 @@
-use std::{
-    io::{self, Read},
-    thread, time,
-};
-use uuid::Uuid;
+use lazy_static::lazy_static;
+use std::sync::Arc;
+use tokio::prelude::*;
+use tokio::sync::{self, oneshot};
 
+const NUM_FUTURES: usize = 10;
+
+lazy_static! {
+    static ref REPL: sync::Mutex<()> = sync::Mutex::new(());
+}
 #[tokio::main]
 async fn main() {
-    println!("When program halts, press enter button to skip debug mode");
-    loop {
-        tokio::spawn(async move {
-            let spawn_id = Uuid::new_v4();
-            println!("Tokio spawned for {:?}", spawn_id);
-            let mut stdin = io::stdin();
-            let _ = stdin.read(&mut [0]).unwrap();
-            println!("Debug mode ended for {:?}", spawn_id);
-        });
-        thread::sleep(time::Duration::from_millis(10));
+    println!("press enter to pause and see the state");
+    let state = Arc::new(sync::Mutex::new(0));
+    for _ in 0..NUM_FUTURES {
+        tokio::spawn(foo(state.clone()));
     }
+    let j = tokio::spawn(async move {
+        loop {
+            let mut stdin = io::stdin();
+            // Future awaits without blocking thread
+            let _ = stdin
+                .read(&mut [0])
+                .await
+                .expect("Failed to read input");
+            let (o_tx, o_rx) = oneshot::channel();
+            tokio::spawn(pause(o_rx));
+            let s = state.lock().await;
+            println!("Paused, current state: {}", s);
+            println!("Press enter to continue");
+            let _ = stdin
+                .read(&mut [0])
+                .await
+                .expect("Failed to read input");
+            o_tx.send(()).ok();
+        }
+    });
+    j.await.ok();
+}
+
+async fn foo(state: Arc<sync::Mutex<u32>>) {
+    loop {
+        // Try to get the lock if it can't then await
+        try_repl().await;
+        let mut s = state.lock().await;
+        *s += 1;
+        tokio::time::delay_for(std::time::Duration::from_millis(10)).await;
+    }
+}
+
+async fn try_repl() {
+    crate::REPL.lock().await;
+}
+
+async fn pause(rx: oneshot::Receiver<()>) {
+    let _h = crate::REPL.lock().await;
+    rx.await.ok();
 }
